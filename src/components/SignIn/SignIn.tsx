a4b8332b9393a0c679250e6138819cae8a9e4aca
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-03-20 16:17:46 +0200

    login corecct

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -16,6 +16,7 @@ const SignIn: React.FC = () => {
 	}, [email, dispatch]);
 
 	const logInUser = async () => {
+		console.log('pass');
 		try {
 			const userReq = await axios.post(
 				'https://backendepicure.onrender.com/users/create/',
@@ -28,7 +29,7 @@ const SignIn: React.FC = () => {
 			dispatch(setActiveUsers(email));
 			setEmail('');
 			setPassword('');
-			// navigate('/');
+			navigate('/');
 		} catch (error: any) {
 			alert(error.response.data);
 			return [];
@@ -55,55 +56,54 @@ const SignIn: React.FC = () => {
 	return (
 		<div id="sign-in-page">
 			{' '}
-			<form onSubmit={handleRegister}>
-				<div id="sign-in-container">
-					<div id="sign-in-title">
-						<div id="sign-in">SIGN IN</div>
-						<div>To continue the order, please sign in</div>
-					</div>
-					<div id="user-information">
-						<input
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							id="input-information"
-							placeholder="Email adress"
-							type={'text'}
-						/>
-						<input
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-							id="input-information"
-							placeholder="Password"
-							type={'password'}
-						/>
-					</div>
-
-					<div id="login-button">
-						<button
-							id="button-login"
-							type="submit">
-							LOGIN
-						</button>
-						<button id="forget-password-button">Forget password?</button>{' '}
-						<div id="or-container">
-							<hr className="hr-left" />
-							<div id="or-letter">or</div>
-							<hr className="hr-right" />
-						</div>
-						<button
-							id="sign-up-button"
-							onClick={() => navigate('/SignUp')}>
-							SIGN UP
-						</button>
-						<button
-							id="add"
-							onClick={add}>
-							{' '}
-							add
-						</button>
-					</div>
+			<div id="sign-in-container">
+				<div id="sign-in-title">
+					<div id="sign-in">SIGN IN</div>
+					<div>To continue the order, please sign in</div>
 				</div>
-			</form>
+				<div id="user-information">
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						id="input-information"
+						placeholder="Email adress"
+						type={'text'}
+					/>
+					<input
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						id="input-information"
+						placeholder="Password"
+						type={'password'}
+					/>
+				</div>
+
+				<div id="login-button">
+					<button
+						onClick={handleRegister}
+						id="button-login"
+						type="submit">
+						LOGIN
+					</button>
+					<button id="forget-password-button">Forget password?</button>{' '}
+					<div id="or-container">
+						<hr className="hr-left" />
+						<div id="or-letter">or</div>
+						<hr className="hr-right" />
+					</div>
+					<button
+						id="sign-up-button"
+						onClick={() => navigate('/SignUp')}>
+						SIGN UP
+					</button>
+					<button
+						id="add"
+						onClick={add}>
+						{' '}
+						add
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
